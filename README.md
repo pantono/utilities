5d580964488c8c500026ee82128fe09b7439fafb
Chris Burton <chris.burton@revolutionbarsgroup.com>
2024-03-29 11:58:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pantono Utilities Module
---
+++ src/EphemeralCacheHelper.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Pantono\Utilities;
+
+class EphemeralCacheHelper
+{
+    /**
+     * @var array<string, mixed>
+     */
+    public static array $cache = [];
+
+    public static function getItem(string $key, callable $data): mixed
+    {
+        if (!isset(self::$cache[$key])) {
+            self::$cache[$key] = $data();
+        }
+
+        return self::$cache[$key];
+    }
+
+    public static function setItem(string $key, mixed $value): void
+    {
+        self::$cache[$key] = $value;
+    }
+
+    public static function clearItem(string $key): void
+    {
+        if (isset(self::$cache[$key])) {
+            unset(self::$cache[$key]);
+        }
+    }
+
+    public static function clearAllCache(): void
+    {
+        self::$cache = [];
+    }
+}
