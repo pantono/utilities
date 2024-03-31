74dd634bc0f79f4a16675219fadd3372e0fff193
Chris Burton <chris.burton@revolutionbarsgroup.com>
2024-03-31 10:41:15 +0100

    ADding reflection helpers

---
+++ src/ReflectionUtilities.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pantono\Utilities;
+
+use Pantono\Contracts\Attributes\FieldName;
+use Pantono\Contracts\Attributes\Filter;
+use Pantono\Contracts\Attributes\Locator;
+use Pantono\Contracts\Attributes\Lazy;
+use ReflectionProperty;
+use ReflectionNamedType;
+
+class ReflectionUtilities
+{
+    /**
+     * @return array{type: ?string, hydrator: ?string, field_name: string, filter: ?string, lazy: ?bool, format: ?string}
+     */
+    public static function parseAttributesIntoConfig(ReflectionProperty $property): array
+    {
+        $info = [
+            'type' => null,
+            'hydrator' => null,
+            'field_name' => StringUtilities::snakeCase($property->getName()),
+            'filter' => null,
+            'lazy' => null,
+            'format' => null
+        ];
+        $type = $property->getType();
+        if ($type instanceof ReflectionNamedType) {
+            $info['type'] = $type->getName();
+        }
+        foreach ($property->getAttributes() as $attribute) {
+            $instance = $attribute->newInstance();
+            if (get_class($instance) === FieldName::class) {
+                $info['field_name'] = $instance->name;
+            }
+            if (get_class($instance) === Filter::class) {
+                $info['filter'] = $instance->filter;
+            }
+            if (get_class($instance) === Locator::class) {
+                $info['hydrator'] = $instance->serviceName . '::' . $instance->methodName;
+            }
+            if (get_class($instance) === Lazy::class) {
+                $info['lazy'] = true;
+            }
+        }
+
+        return $info;
+    }
+}
